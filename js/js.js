8135f2d493cf8fa382428697814cce31e6bcf134
nigirock <nigirock124@gmail.com>
2015-07-08 13:46:15 +0300

    10

--- js/js.js
+++ js/js.js
@@ -1,1 +1,7 @@
-
+$( document ).ready(function() {
+    var btnSearch =  $(".btn");
+    var formSearch = $(".form-control");
+    btnSearch.on("click",function(){
+       formSearch.toggleClass("block-input-group").focus();
+    });
+});
